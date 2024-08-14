bdf97f24f3eb00a45c4d571eec36dd3138e2efb1
namiron <sasha_zpua@mail.ru>
2024-08-14 14:00:45 +0300

    fix_errors

---
+++ src/command/constant.js
@@ -0,0 +1,36 @@
+export const RESULT = "Restart";
+
+export const questions = [
+  {
+    title: "React is a ...?",
+    variables: ["library", "framework", "language"],
+    correct: 0,
+  },
+  {
+    title: "A component is a ...?",
+    variables: [
+      "function that returns text",
+      "function that returns JSX",
+      "function that returns nothing",
+    ],
+    correct: 1,
+  },
+  {
+    title: "What is useState?",
+    variables: [
+      "A function for saving state",
+      "A global state",
+      "An unnecessary function",
+    ],
+    correct: 0,
+  },
+  {
+    title: "What is JSX?",
+    variables: [
+      "A syntax for JavaScript code",
+      "A function name",
+      "A syntax for HTML-like code",
+    ],
+    correct: 2,
+  },
+];
